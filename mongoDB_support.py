99be079dd273202a0bcb34796133e05ed3182aca
difftime <1160289632@qq.com>
2017-06-21 20:28:12 +0800

    first commit

--- mongoDB_support.py
+++ mongoDB_support.py
@@ -1,14 +1,52 @@
-from pymongo import MongoClient  
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+try:
+	from pymongo import MongoClient
+except ImportError:
+	print("failed to import pymongo,please install it!")
+	exit(1)	 # 检查是否安装并运行成功pymongo，未成功则退出
+
 from bson.objectid import ObjectId  
 from gridfs import *
-def read(file_id,offset,size):  
-    client = MongoClient()  
-    db = client.gdbfs 
-    fs = GridFS(db, 'col') 
-    file = fs.get_version(file_id, 0)  
-    if file:
-	file.seek(offset)
-	return file.read(size)
-    else:
-	print("connot find this file!")
-	exit(-1)
+
+class mongo_file:
+
+	def read(file_name,offset,size):  #读文档
+    		client = MongoClient()  
+    		db = client.gdbfs 
+    		fs = GridFS(db, 'col') 
+   		file = fs.get_version(file_id, 0) 
+
+ 
+    		if file:
+			file.seek(offset)
+			return file.read(size)
+			#返回文件内容
+    		
+		else:
+		#数据库中不存在该文件
+			print("connot find this file!")
+			exit(-1)
+
+
+
+	def write(data,file_name):   #写文档
+    		client = MongoClient()  
+    		db = client.gdbfs
+    		fs = GridFS(db, 'col') 
+       		id = fs.put(data,filename=file_name)  
+
+       		return id
+
+
+
+	def insert(file_name):
+    		client = MongoClient()  
+    		db = client.test_db
+    		fs = GridFS(db, 'col') 
+    		with open (file_name.decode('utf-8'),'rb') as myimage:  
+       			data=myimage.read()
+       			id = fs.put(data,filename=file_name) 
+ 
+       		return id
