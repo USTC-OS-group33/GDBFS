490e731e20e7b7026099be0179575c1fde87b573
difftime <1160289632@qq.com>
2017-06-21 20:38:08 +0800

    first commit

--- fake_database_support/mongoDB_support.py
+++ fake_database_support/mongoDB_support.py
@@ -1,19 +1,17 @@
 #-*- coding:utf-8 -*-
 
 import os
-# from pymongo import 
-# from gridfs import 
-# from bson.code import 
-# from pymongo.errors import ConfigurationError
-# import some packges
+from pymongo import 
+from gridfs import 
+from bson.code import 
+from pymongo.errors import ConfigurationError
+import some packges
+
 
 fake_mongoDB='fake_database/fake_mongoDB'
 
 
 class File:
-
-    def __init__(self, file_id):
-        self.path = fake_mongoDB + '/' + str(file_id)
     
     def read(self, size, offset):
             fh = open(self.path, "rb")
@@ -38,9 +36,3 @@ class File:
 def get_file(node_id):
     file = File(node_id)
     return file
-
-def create(node_id):
-    pass
-
-def write(node_id, data, offset):
-    pass
