c770436a6ee914350bf981612daca13207b00f38
difftime <1160289632@qq.com>
2017-06-22 16:37:00 +0800

    first commit

--- mongoDB_support.py
+++ mongoDB_support.py
@@ -5,33 +5,36 @@ try:
 	from pymongo import MongoClient
 except ImportError:
 	print("failed to import pymongo,please install it!")
-	exit(1)	 # 检查是否安装并运行成功pymongo，未成功则退出
+	exit(1)	
+	 # 检查是否安装并运行成功pymongo，未成功则退出
 
 from bson.objectid import ObjectId  
 from gridfs import *
 
 class mongo_file:
 
-	def read(file_name,offset,size):  #读文档
-    		client = MongoClient()  
+	def read(file_name,offset,size): 
+		#读文档
+        	client = MongoClient()  
     		db = client.gdbfs 
     		fs = GridFS(db, 'col') 
-   		file = fs.get_version(file_id, 0) 
+   		temp = fs.get_version(file_name, 0) 
 
  
-    		if file:
-			file.seek(offset)
-			return file.read(size)
+    		if temp:
+			temp.seek(offset)
+			return temp.read(size)
 			#返回文件内容
     		
 		else:
-		#数据库中不存在该文件
+			#数据库中不存在该文件
 			print("connot find this file!")
 			exit(-1)
 
 
 
-	def write(data,file_name):   #写文档
+	def write(data,file_name):   
+		#写文档
     		client = MongoClient()  
     		db = client.gdbfs
     		fs = GridFS(db, 'col') 
@@ -41,7 +44,8 @@ class mongo_file:
 
 
 
-	def insert(file_name):   #本地上传文件
+	def insert(file_name):   
+		#本地上传文件
     		client = MongoClient()  
     		db = client.gdbfs
     		fs = GridFS(db, 'col') 
@@ -52,10 +56,36 @@ class mongo_file:
        		return id
 	
 	
-	def existfile(file_id):
+	def existfile(file_id):		
+		#检查是否存在文档
 		client = MongoClient()  
 		db = client.gdbfs
 		fs = GridFS(db, 'col') 
 		t=fs.exists(ObjectId(file_id))
 	
 		return t
+
+	def length(file_name):  
+		#获得文件大小
+    		client= MongoClient()
+    		db = client.gdbfs
+    		fs = GridFS(db, 'col')
+    		file = fs.get_version(file_name, 0)  
+
+    		if file:
+			return file.length
+
+    		else :
+    		#找不到文件	
+			print("this file doesn't exist! please check it again!")
+
+
+			exit(-1)
+
+	def delFile(file_id):  
+		#删除文件
+    		client = MongoClient() 
+    		db = client.test_db  
+    		fs = GridFS(db, 'col')  
+    		fs.delete(ObjectId(file_id))  
+
