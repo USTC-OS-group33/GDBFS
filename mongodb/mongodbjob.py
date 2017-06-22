9d186acc95d83bb340875616b22f458891b68b7a
difftime <1160289632@qq.com>
2017-06-22 16:44:04 +0800

    first commit

--- mongodb/mongodbjob.py
+++ mongodb/mongodbjob.py
@@ -1,8 +1,7 @@
 # This Python file uses the following encoding: utf-8
 
-from pymongo import Connection
+from pymongo import MongoClient
 from gridfs import *
-from PIL import Image
 from bson.objectid import ObjectId
 import StringIO
 import threading, time
@@ -19,9 +18,9 @@ class GFS:
     @staticmethod
     def _connect():
         if  not GFS.db:
-	    client = MongoClient()  # 建立mongodb的连接"
-   	    GFS.db = client.admin #连接到指定的数据库中
-        GFS.fs = GridFS(GFS.db,  collection='images') #连接到具体的collection中
+	    	client = MongoClient()  # 建立mongodb的连接"
+   	    	GFS.db = client.gdbfs #连接到指定的数据库中
+        	GFS.fs = GridFS(GFS.db,  collection='col') #连接到具体的collection中
  
  
     #初始化
@@ -55,15 +54,14 @@ class GFS:
    	    image = Image.open(name)
             image.save(data,format)
             #print "name is %s=======data is %s" % (name, data.getvalue())
-        gf = GFS.fs.put(data.getvalue(), filename=name, format=format)
+            GFS.fs.put(data.getvalue(), filename=name, format=format)
 	except Exception as e:
    		print "Exception ==>> %s " % e
         finally:
             GFS.c = None
             GFS._connect()
  
- 
-        return gf
+
  
  
     #获得图片
@@ -79,8 +77,8 @@ class GFS:
             dic["upload_date"] = gf.upload_date
             dic["name"] = gf.name
             dic["content_type"] = gf.content_type
-   	dic["format"] = gf.format
-        return (im , dic)
+   	    dic["format"] = gf.format
+            return (im , dic)
         except Exception,e:
             print e
             return (None,None)
@@ -92,27 +90,15 @@ class GFS:
     #将gridFS中的图片文件写入硬盘
     def write_2_disk(self, data, dic):
         name = "./get_%s" % dic['name']
-if name:
-            output = open(name, 'wb')
-   output.write(data)
-   output.close()
-   print "fetch image ok!"
+	if name:
+            	output = open(name, 'wb')
+   		output.write(data)
+   		output.close()
+   		print "fetch image ok!"
  
     #获得文件列表
     def list(self):
         return GFS.fs.list()
  
  
-    #删除文件
-    def remove(self,name):
-        GFS.fs.remove(name)
- 
-if __name__== '__main__':
-image_name= raw_input("input the image name>>")
-if image_name:
-           gfs = GFS.getInstance()
-           if gfs:
-                image_id = gfs.put(image_name)
-                print "==========Object id is %s  and it's type is %s==========" % (image_id , type(image_id))
-(data, dic) = gfs.get(ObjectId(image_id))
-gfs.write_2_disk(data, dic)
+
